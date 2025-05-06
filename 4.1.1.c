b5b5b80d2be8101532e2e26ed532308cda9e6e47
chintan123-a <chintanbhalala13@gmail.com>
2025-05-06 12:52:21 +0530

    Add files via upload

---
+++ 4.1.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,result;
+	
+	printf("enter value of x: ");
+	scanf("%d",&x);
+	
+	printf("enter value of y: ");
+	scanf("%d",&y);
+	
+	result=(x+y)*(x+y);
+	
+	printf("result of (x+y)*(x+y) is:%d\n",result);
+	
+	
+}
