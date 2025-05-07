4859e61493e0e2cae03d8ac693899baf7688cb1f
chintan123-a <chintanbhalala13@gmail.com>
2025-05-07 15:10:33 +0530

    Add files via upload

---
+++ (h.w.5.1.1.a).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{ 
+    int a;
+    
+    printf("Enter a number:a");
+    scanf("%d" , &a);
+    
+    if (a<0){
+	     printf("The number is positive.\n");
+    }
+}
