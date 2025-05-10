77dcf9710e04c29ea45cf1a78173fe4745f6bd4c
chintan123-a <chintanbhalala13@gmail.com>
2025-05-10 11:11:26 +0530

    Add files via upload

---
+++ c.h.1.1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+    int score,
+        grade;
+    
+    printf("enter the score: ");
+    scanf("%d" , &score);
+    
+    if(score>=90)
+    { 
+       printf("your grade is A");
+    }
+    
+    else if(score>=80)
+    { 
+       printf("your grade is B");
+    }
+    
+    else if(score>=70)
+    { 
+       printf("your grade is C");
+    }
+    
+    else if(score>=60)
+    { 
+       printf("your grade is D");
+    }
+    
+    else if(score>=50)
+    { 
+       printf("your grade is E");
+    }
+    
+    else if(score>=40)
+    { 
+       printf("your grade is F");
+    }
+    
+    printf("%c\n",grade89);
+}
