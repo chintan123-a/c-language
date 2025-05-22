24030a18cc102168bd018c399fb61e498e7cda83
chintan123-a <chintanbhalala13@gmail.com>
2025-05-22 12:31:10 +0530

    Add files via upload

---
+++ 16 may(7).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int rows = 5;
+	
+	for (int i= 0; i < rows; i++)
+	{
+		for (int j = i; j >= 0; j--)
+		{
+		    printf("%c", 'A' + j);
+		}
+		printf("\n");
+	}
+}
